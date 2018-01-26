402a0a73b72580a6cf9302e121f1c64b6516c61d
Remi A. Solås <remi@npolar.no>
2018-01-26 15:36:01 +0100

    Stylistic code changes

--- src/app.js
+++ src/app.js
@@ -5,21 +5,19 @@ var npdcCommon = require("npdc-common");
 
 /*
 var leaflet = require("npdc-common/src/wrappers/leaflet");
-var npdcPbhimsApp = angular.module("npdcPbhimsApp", ["npdcCommon", "leaflet"]);
+var npdcPbhimsApp = angular.module("npdcPbhimsApp", [ "npdcCommon", "leaflet" ]);
 */
 
-var npdcPbhimsApp = angular.module("npdcPbhimsApp", ["npdcCommon"]);
+var npdcPbhimsApp = angular.module("npdcPbhimsApp", [ "npdcCommon" ]);
 npdcPbhimsApp.controller("PbhimsShowController", require("./controller/pbhims-show"));
 npdcPbhimsApp.controller("PbhimsEditController", require("./controller/pbhims-edit"));
 npdcPbhimsApp.controller("PbhimsSearchController", require("./controller/pbhims-search"));
 
 var resources = [
-  {"path": "/polar-bear/incident", "resource": "PbhimsResource", "uiBase": "/pbhims"}
+  { "path": "/polar-bear/incident", "resource": "PbhimsResource", "uiBase": "/pbhims" }
 ];
 
-resources.forEach(service => {
-  return npdcPbhimsApp.factory(service.resource, ["NpolarApiResource", NpolarApiResource => NpolarApiResource.resource(service) ]);
-});
+resources.forEach(service => npdcPbhimsApp.factory(service.resource, [ "NpolarApiResource", NpolarApiResource => NpolarApiResource.resource(service) ]));
 
 npdcPbhimsApp.config(require("./router"));
 
