3e837c5587ddfcf9a8e4843d3d0c697aac2278e8
Remi A. Solås <remi@npolar.no>
2018-01-26 15:39:06 +0100

    Added formula

--- src/controller/pbhims-edit.js
+++ src/controller/pbhims-edit.js
@@ -1,15 +1,27 @@
 "use strict";
 
-function PbhimsEditController($scope, $controller, NpolarLang, formula, PbhimsResource) {
+function PbhimsEditController($scope, $controller, NpolarLang, npdcAppConfig, formula, chronopicService, PbhimsResource) {
   "ngInject";
   
   $controller("NpolarEditController", { $scope: $scope });
   $scope.resource = PbhimsResource;
   
   $scope.formula = formula.getInstance({
-    schema: "//api.npolar.no/schema/polarbear-incident-0.1.0.json",
-    language: NpolarLang.getLang()
+    schema: "//api.npolar.no/schema/polarbear-incident-1.1.0.json",
+    form: "config/formula.json",
+    language: NpolarLang.getLang(),
+    templates: npdcAppConfig.formula.templates.concat([{
+      match: "location.name",
+      template: "<npdc:formula-placename></npdc:formula-placename>"
+    }])
   });
+
+  /*
+  chronopicService.defineOptions({
+    match: (field) => field.path.match(/datetime/),
+    format: "{date}"
+  });
+  */
 
   $scope.edit();
 }
